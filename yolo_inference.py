44582e709905149170c521caa94942a0d8084368
09Kanika <kanika2005dogra@gmail.com>
2024-11-04 17:03:26 +0530

    Updated files

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x')
 
-result = model.track('input_media/video1.mp4',conf=0.2, save=True)
+result = model.track('input_media/image1.png',conf=0.2, save=True)
 print(result)
 print("boxes:")
 for box in result[0].boxes:
